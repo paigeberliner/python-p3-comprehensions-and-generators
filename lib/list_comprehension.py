bec49c5dab637567c25a63dd476dc95c96f80aeb
Paige <paigeberliner@gmail.com>
2024-02-18 14:28:28 -0500

    Complete Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = []
+    for number in num_list:
+        if number % 2 == 0:
+            even_numbers.append(number)
+
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    modified_list = []
+    for sentence in sentence_list:
+        modified_sentence = sentence + "!"
+        modified_list.append(modified_sentence)
+
+    return modified_list
